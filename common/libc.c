5bb20057c2695e5b93b50db6de3004ede6eddbde
Elroi Allouch <elro444@gmail.com>
2024-06-02 20:28:59 +0300

    Organize libc files

---
+++ common/libc.c
@@ -0,0 +1,48 @@
+#include "libc.h"
+
+
+void memmove(u8 *dst, const u8 *src, u32 size)
+{
+    if (src > dst) {
+        for (u32 i = 0; i < size; ++i) {
+            dst[i] = src[i];
+        }
+    }
+    else {
+        for (u32 i = size - 1; i >= 0; --i) {
+            dst[i] = src[i];
+        }
+    }
+}
+
+int strcmp(const char *a, const char *b)
+{
+    while (*a && *b) {
+        if (*a != *b) {
+            break;
+        }
+        ++a;
+        ++b;
+    }
+    return *a - *b;
+}
+
+void strcat(char *a, const char *b)
+{
+    while (*a) {
+        ++a;
+    }
+    while (*b) {
+        *a++ = *b++;
+    }
+    *a = 0;
+}
+
+unsigned strlen(char *str)
+{
+    unsigned length = 0;
+    while (*str++) {
+        ++length;
+    }
+    return length;
+}
--- include/libc.h
+++ include/libc.h
@@ -3,43 +3,17 @@
 
 #include "types.h"
 
-static inline int strcmp(const char *a, const char *b)
-{
-    while (*a && *b) {
-        if (*a != *b) {
-            break;
-        }
-        ++a;
-        ++b;
-    }
-    return *a - *b;
-}
+int strcmp(const char *a, const char *b);
+void strcat(char *a, const char *b);
+unsigned strlen(char *str);
 
-static inline void strcat(char *a, const char *b)
-{
-    while (*a) {
-        ++a;
-    }
-    while (*b) {
-        *a++ = *b++;
-    }
-    *a = 0;
-}
-
-static inline unsigned strlen(char *str)
-{
-    unsigned length = 0;
-    while (*str++) {
-        ++length;
-    }
-    return length;
-}
-
-static inline void memcpy(u8 *src, u8 *dst, u32 size)
+static inline void memcpy(u8 *dst, const u8 *src, u32 size)
 {
     for (u32 i = 0; i < size; ++i) {
         dst[i] = src[i];
     }
 }
+
+void memmove(u8 *dst, const u8 *src, u32 size);
 
 #endif /* LIBC_H */
