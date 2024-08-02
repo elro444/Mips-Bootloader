18e61e73198aa9b2125dd1bb968710b5edd60ca5
Elroi Allouch <elro444@gmail.com>
2024-08-03 01:27:56 +0300

    Add more libc functions

--- common/libc.c
+++ common/libc.c
@@ -17,6 +17,24 @@ void memmove(u8 *dst, const u8 *src, u32 size)
     }
 }
 
+void memset(u8 *dst, u8 value, u32 size)
+{
+    while (size-- > 0)
+        *dst++ = value;
+}
+
+int memcmp(const u8 *a, const u8 *b, u32 size)
+{
+    while (size > 0) {
+        if (*a != *b)
+            return *a - *b;
+        ++a;
+        ++b;
+        --size;
+    }
+    return 0;
+}
+
 int strcmp(const char *a, const char *b)
 {
     while (*a && *b) {
@@ -48,3 +66,50 @@ unsigned strlen(char *str)
     }
     return length;
 }
+
+int strchr(const char *str, const char c)
+{
+    int index = 0;
+    while (*str) {
+        if (*str++ == c)
+            return index;
+        ++index;
+    }
+    return -1;
+}
+
+char *strstr(const char *haystack, const char *needle)
+{
+    u32 needle_len = strlen(needle);
+    while (*haystack) {
+        if (0 == memcmp(haystack, needle, needle_len))
+            return haystack;
+    }
+    return NULL;
+}
+
+char *strtok(const char *str, const char *delim, char **saveptr)
+{
+    unsigned delim_length = strlen(delim);
+    if (NULL != str) {
+        // First call, start from the beginning
+        *saveptr = str;
+    }
+    else {
+        str = *saveptr;
+    }
+    
+    if (NULL == str)
+        return NULL;
+
+    char *next_delim = strstr(str, delim);
+    if (NULL != next_delim) {
+        memset(next_delim, 0, delim_length);
+        *saveptr = next_delim + delim_length;
+    } else {
+        *saveptr = NULL;
+    }
+    
+    return str;
+}
+
--- include/libc.h
+++ include/libc.h
@@ -6,6 +6,9 @@
 int strcmp(const char *a, const char *b);
 void strcat(char *a, const char *b);
 unsigned strlen(char *str);
+int strchr(const char *str, const char c);
+char *strstr(const char *haystack, const char *needle);
+char *strtok(const char *str, const char *delim, char **saveptr);
 
 static inline void memcpy(u8 *dst, const u8 *src, u32 size)
 {
@@ -15,5 +18,7 @@ static inline void memcpy(u8 *dst, const u8 *src, u32 size)
 }
 
 void memmove(u8 *dst, const u8 *src, u32 size);
+void memset(u8 *dst, u8 value, u32 size);
+int memcmp(const u8 *a, const u8 *b, u32 size);
 
 #endif /* LIBC_H */
